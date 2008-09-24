9381c8e6ed31deb0ee758ec6ed54875e81a55458
raydenx <raydenx@localhost>
2008-09-24 16:54:45 +0000

    array macro fix

--- OpenICF-java-framework/test-contract/src/org/identityconnectors/contract/data/macro/ArrayMacro.java
+++ OpenICF-java-framework/test-contract/src/org/identityconnectors/contract/data/macro/ArrayMacro.java
@@ -39,69 +39,68 @@
  */
 package org.identityconnectors.contract.data.macro;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.contract.exceptions.ContractException;
 import org.junit.Assert;
 
 /**
+ * This macro takes at least one parameter - class name of type of the array, and then array values.
  * 
  * @author David Adam
- *
+ * @author Zdenek Louzensky
+ * 
  */
 public class ArrayMacro implements Macro {
 
-	private static final Log LOG = Log.getLog(ArrayMacro.class);
-	// number of mandatory parameters
-	private static final int NR_OF_MANDATORY_PARAMS = 2;
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getName() {
-		return "ARRAY";
-	}
+    private static final Log LOG = Log.getLog(ArrayMacro.class);
+    // number of mandatory parameters
+    private static final int NR_OF_MANDATORY_PARAMS = 2;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Object resolve(Object[] parameters) {
-		
-		List<Object> ret = new ArrayList<Object>();
-		
-		LOG.ok("enter");
-		
-		//number of parameters should be at least 2
-		Assert.assertTrue(NR_OF_MANDATORY_PARAMS <= parameters.length);
-		
-		// first parameter (mandatory) is macro name
-		Assert.assertEquals(parameters[0], getName());
-		
-		// second parameter (mandatory) is the array type
-		Assert.assertTrue(parameters[1] instanceof Object);
-		String inClazz = (String) parameters[1];
-		
-		try {
-			//TODO thorough testing of this is needed
-			Class clazz = Class.forName(inClazz);
-			Constructor c = clazz.getConstructor(clazz);
+    /**
+     * {@inheritDoc}
+     */
+    public String getName() {
+        return "ARRAY";
+    }
 
-			// the other parameters (voluntary) represent the items of the array
-			for (int i = NR_OF_MANDATORY_PARAMS; i < parameters.length; i++) {
-				ret.add(parameters[i]);
-			}
-			
-			LOG.ok("''{0}'' macro was run", getName());
-		} catch (Exception ex) {
-			LOG.error(ex,
-					"Unable to process the Array macro with class: ''{0}''",
-					inClazz);
-		}
-		
-		// in case no items are given, an empty object array is returned.
-		return ret.toArray(new Object[0]);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public Object resolve(Object[] parameters) {
+        LOG.ok("enter");
+
+        // number of parameters should be at least 2
+        Assert.assertTrue(NR_OF_MANDATORY_PARAMS <= parameters.length);
+
+        // first parameter (mandatory) is macro name
+        Assert.assertEquals(parameters[0], getName());
+
+        // second parameter (mandatory) is the array type name
+        Assert.assertTrue(parameters[1] instanceof String);
+        String inClazz = (String) parameters[1];
+
+        try {
+            Class<?> clazz = Class.forName(inClazz);
+            Constructor<?> c = clazz.getConstructor(String.class);
+            // create array instance
+            Object array = Array.newInstance(clazz, parameters.length - NR_OF_MANDATORY_PARAMS);
+            // set values in the array
+            for (int i = NR_OF_MANDATORY_PARAMS; i < parameters.length; i++) {
+                Array.set(array, i - NR_OF_MANDATORY_PARAMS, c.newInstance(parameters[i]));
+            }
+
+            return array;
+
+        } catch (Exception ex) {
+            LOG.error(ex,
+                    "Unable to process the Array macro with parameters: ''{0}''",
+                    Arrays.asList(parameters));
+            throw ContractException.wrap(ex);
+        }
+    }
 
 }
