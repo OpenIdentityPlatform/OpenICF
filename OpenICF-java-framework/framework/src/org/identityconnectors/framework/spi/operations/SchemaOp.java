9e3f9a08ac83357be21371c2c30e4f8e92e9d97d
garypcole <garypcole@localhost>
2008-11-09 03:25:25 +0000

    Javadoc.

--- OpenICF-java-framework/framework/src/org/identityconnectors/framework/spi/operations/SchemaOp.java
+++ OpenICF-java-framework/framework/src/org/identityconnectors/framework/spi/operations/SchemaOp.java
@@ -40,20 +40,31 @@
 package org.identityconnectors.framework.spi.operations;
 
 import org.identityconnectors.framework.common.objects.Schema;
+import org.identityconnectors.framework.common.objects.filter.AbstractFilterTranslator;
 import org.identityconnectors.framework.spi.Connector;
 
+/**
+ * Implement this interface to allow the Connector to describe which types of objects
+ * the Connector manages on the target resource (and which operations
+ * and which options the Connector supports for each type of object).
+ * @param T The result type of the translator. 
+ * @see AbstractFilterTranslator For more information
+ */
 public interface SchemaOp extends SPIOperation {
 
     /**
-     * Determines what types of objects this {@link Connector} supports. This
+     * Describes the types of objects this {@link Connector} supports. This
      * method is considered an operation since determining supported objects may
      * require configuration information and allows this determination to be
      * dynamic.
      * <p>
-     * The special {@link Uid} attribute should never appear in the schema, as it is not a
-     * true attribute of an object, only a reference to it. If your resource does have a writable
-     * unique id attribute that is different than its {@link Name}, you should create a resource-specific attribute
-     * to map to that, such as <I>unix_uid</I>.
+     * The special {@link org.identityconnectors.framework.common.objects.Uid} attribute 
+     * should never appear in the schema, as it is not a true attribute of an object, 
+     * rather a reference to it. If your resource object-class has a writable unique id attribute 
+     * that is different than its {@link org.identityconnectors.framework.common.objects.Name}, 
+     * then your schema should contain a resource-specific attribute that represents this unique id.
+     * For example, a Unix account object might contain <I>unix_uid</I>.
+     * 
      * @return basic schema supported by this {@link Connector}.
      */
     Schema schema();
--- OpenICF-java-framework/framework/src/org/identityconnectors/framework/spi/operations/SearchOp.java
+++ OpenICF-java-framework/framework/src/org/identityconnectors/framework/spi/operations/SearchOp.java
@@ -54,9 +54,11 @@ import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
 public interface SearchOp<T> extends SPIOperation {
     
     /**
-     * Creates a filter translator that will translate a specified filter to the
-     * native filter. The translated filters will be subsequently passed to
-     * {@link #search(ObjectClass, Object, ResultsHandler)}
+     * Creates a filter translator that will translate a specified 
+     * {@link org.identityconnectors.framework.common.objects.filter.Filter filter} 
+     * into one or more native queries.
+     * Each of these native queries will be passed subsequently into
+     * <code>executeQuery()</code>.
      * 
      * @param oclass
      *            The object class for the search. Will never be null.
@@ -65,27 +67,33 @@ public interface SearchOp<T> extends SPIOperation {
      *            If the caller passes null, the framework will convert this
      *            into an empty set of options, so SPI need not worry about this
      *            ever being null.
-     * @return A filter translator this must not be <code>null</code>. Or the
-     *         API will throw a {@link NullPointerException}.
+     * @return A filter translator. This must not be <code>null</code>. 
+     *  	   A <code>null</code> return value will cause the API 
+     *         (<code>SearchApiOp</code>) to throw {@link NullPointerException}.
      */
     public FilterTranslator<T> createFilterTranslator(ObjectClass oclass, OperationOptions options);
+    
     /**
-     * This will be called by ConnectorFacade, once for each native query produced
-     * by the FilterTranslator. If there is more than one query the results will
-     * automatically be merged together and duplicates eliminated. NOTE
-     * that this implies an in-memory data structure that holds a set of
-     * Uids, so memory usage in the event of multiple queries will be O(N)
-     * where N is the number of results. That is why it is important that
-     * the FilterTranslator implement OR if possible.
+     * ConnectorFacade calls this method once for each native query 
+     * that the {@linkplain #createFilterTranslator(ObjectClass, OperationOptions) FilterTranslator} 
+     * produces in response to the <code>Filter</code> passed into 
+     * {@link org.identityconnectors.framework.api.operations.SearchApiOp#search SearchApiOp}.
+     * If the <code>FilterTranslator</code> produces more than one native query, then ConnectorFacade
+     * will automatically merge the results from each query and eliminate any duplicates.
+     * NOTE that this implies an in-memory data structure that holds a set of
+     * Uid values, so memory usage in the event of multiple queries will be O(N)
+     * where N is the number of results. This is why it is important that
+     * the FilterTranslator for each Connector implement OR if possible.
+     * 
      * @param oclass The object class for the search. Will never be null.
-     * @param query The native query to run. A value of null means 'return everything for the given object class'.
+     * @param query The native query to run. A value of null means 
+     * 			  "return every instance of the given object class".
      * @param handler
      *            Results should be returned to this handler
      * @param options
-     *            additional options that impact the way this operation is run.
+     *            Additional options that impact the way this operation is run.
      *            If the caller passes null, the framework will convert this into
-     *            an empty set of options, so SPI need not worry
-     *            about this ever being null.
+     *            an empty set of options, so SPI need not guard against options being null.
      */
     public void executeQuery(ObjectClass oclass, T query, ResultsHandler handler, OperationOptions options);
 
