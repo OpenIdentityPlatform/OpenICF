c6c573000bd5a819ecc070c11e08004cbf44d413
roman_kitko <roman_kitko@localhost>
2009-02-24 16:37:24 +0000

    Find common localized messages for DB2 and Oracle

---
+++ OpenICF-dbcommon/src/org/identityconnectors/dbcommon/DBMessages.java
@@ -0,0 +1,9 @@
+package org.identityconnectors.dbcommon;
+
+class DBMessages {
+    static final String ASSERT_NULL = "assert.null";
+    static final String ASSERT_NOT_NULL = "assert.notNull";
+    static final String ASSERT_BLANK = "assert.blank";
+    static final String ASSERT_NOT_BLANK = "assert.notBlank";
+
+}
---
+++ OpenICF-dbcommon/src/org/identityconnectors/dbcommon/LocalizedAssert.java
@@ -0,0 +1,93 @@
+package org.identityconnectors.dbcommon;
+
+import org.identityconnectors.framework.common.objects.ConnectorMessages;
+
+/**
+ * Localized asserts is a set of localized asserts utility method that throws localized
+ * exception when assert is not true
+ * 
+ * @author kitko
+ *
+ */
+public class LocalizedAssert {
+
+    private ConnectorMessages cm;
+
+    /**
+     * Creates asserts with messages
+     * @param cm
+     * @throws IllegalArgumentException if cm param is null
+     */
+    public LocalizedAssert(ConnectorMessages cm){
+        if(cm == null){
+            throw new IllegalArgumentException("ConnectorMessages argument is null");
+        }
+        this.cm = cm;
+    }
+    
+    private void throwException(String locKey,String argument){
+        String msg = cm.format(locKey, null, argument);
+        throw new IllegalArgumentException(msg);
+    }
+    
+    /**
+     * Asserts the argument is not null. If argument is null, throws localized
+     * IllegalArgumentException
+     * @param <T>
+     * @param o
+     * @param argument
+     * @return o param
+     */
+    public <T> T assertNotNull(T o, String argument) {
+        if (o == null) {
+            throwException(DBMessages.ASSERT_NOT_NULL,argument);
+        }
+        return o;
+    }
+    
+    /**
+     * Asserts the argument is null. If argument is not null, throws localized
+     * IllegalArgumentException
+     * @param <T>
+     * @param o
+     * @param argument
+     * @return o
+     */
+    public <T> T assertNull(T o,String argument){
+        if(o != null){
+            throwException(DBMessages.ASSERT_NULL,argument);
+        }
+        return o;
+    }
+    
+    /**
+     * Asserts that argument string is not blank. If argument is null or blank, throws localized
+     * IllegalArgumentException
+     * @param o
+     * @param argument
+     * @return same string
+     */
+    public String assertNotBlank(String o, String argument) {
+        if (o == null || o.length() == 0) {
+            throwException(DBMessages.ASSERT_NOT_BLANK,argument);
+        }
+        return o;
+    }
+    
+    /**
+     * Asserts that argument string is blank. If argument is not blank, throws localized
+     * IllegalArgumentException
+     * @param s
+     * @param argument
+     * @return same string
+     */
+    public String asserBlank(String s,String argument){
+        if(s != null && s.length() > 0){
+            throwException(DBMessages.ASSERT_BLANK,argument);
+        }
+        return s;
+    }
+    
+    
+    
+}
