096aa9b1a082732823a9aeb96ab85243964198ce
abadea <abadea@localhost>
2009-02-25 15:38:54 +0000

    Removing unused import

--- OpenICF-ldap-connector/src/org/identityconnectors/ldap/AdapterCompatibilityTests.java
+++ OpenICF-ldap-connector/src/org/identityconnectors/ldap/AdapterCompatibilityTests.java
@@ -25,7 +25,6 @@ package org.identityconnectors.ldap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.util.EnumSet;
 import java.util.HashSet;
