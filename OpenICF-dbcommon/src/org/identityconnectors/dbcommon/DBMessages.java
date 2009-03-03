94e2c39057498399ae47f57be331a6f72fbbb060
roman_kitko <roman_kitko@localhost>
2009-03-03 09:56:49 +0000

    Make DbMessages abstract

--- OpenICF-dbcommon/src/org/identityconnectors/dbcommon/DBMessages.java
+++ OpenICF-dbcommon/src/org/identityconnectors/dbcommon/DBMessages.java
@@ -1,9 +1,8 @@
 package org.identityconnectors.dbcommon;
 
-class DBMessages {
+abstract class DBMessages {
     static final String ASSERT_NULL = "assert.null";
     static final String ASSERT_NOT_NULL = "assert.notNull";
     static final String ASSERT_BLANK = "assert.blank";
     static final String ASSERT_NOT_BLANK = "assert.notBlank";
-
 }
