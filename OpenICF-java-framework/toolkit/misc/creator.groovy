c3ca77deebac0c6f2e2fc85456ebd4fd62ecab57
abadea <abadea@localhost>
2009-08-20 09:46:10 +0000

    Putting the connector test class in src/test/java

--- OpenICF-java-framework/toolkit/misc/creator.groovy
+++ OpenICF-java-framework/toolkit/misc/creator.groovy
@@ -72,12 +72,15 @@ jarVersion = "1.0"
 //map that the template engine will use
 values = ["userName":properties["user.name"], "packageName":PACKAGE_NAME, "resourceName":RESOURCE_NAME, "frameworkDir":baseDir, "bundleDir":BUNDLE_DIR, "interfaces":interfaces, "jarVersion":jarVersion]
 templates = new File(baseDir, "templates").listFiles().toList()
+print templates
 
-srcPath = BUNDLE_DIR + "/src/main/java/" + PACKAGE_NAME.replaceAll("\\.", "/") + "/"
+packagePath = PACKAGE_NAME.replaceAll("\\.", "/") + "/"
+srcPath = BUNDLE_DIR + "/src/main/java/" + packagePath
+testPath = BUNDLE_DIR + "/src/test/java/" + packagePath
 
 //create directories
 ant.mkdir(dir:srcPath)
-ant.mkdir(dir:BUNDLE_DIR + "/src/test/java")
+ant.mkdir(dir:testPath)
 ant.mkdir(dir:BUNDLE_DIR + "/config")
 ant.mkdir(dir:BUNDLE_DIR + "/lib/build")
 ant.mkdir(dir:BUNDLE_DIR + "/lib/test")
@@ -90,7 +93,9 @@ templates.each {
     if(fName.endsWith(".template")) {
         fName = fName.substring(0, fName.length() - ".template".length());
     }
-    if(fName.endsWith(".java")) {
+    if(fName.endsWith("Tests.java")) {
+        f = new File(testPath, RESOURCE_NAME + fName)
+    }else if(fName.endsWith(".java")) {
         f = new File(srcPath, RESOURCE_NAME + fName)
     }else if(fName.startsWith("Messages")) {
         f = new File(srcPath, fName)
