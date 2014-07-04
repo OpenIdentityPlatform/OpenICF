abdf25ed3b18dfac58465430a85b0f3ce8b46f03
Laszlo Hordos <Laszlo.Hordos@forgerock.com>
2014-07-04 14:07:00 +0000

    Update the verify script in integration test

--- OpenICF-maven-plugin/src/it/docbook-connector/verify.groovy
+++ OpenICF-maven-plugin/src/it/docbook-connector/verify.groovy
@@ -23,8 +23,8 @@
  */
 
 assert new File( basedir, 'target/openicf-docbkx' ).exists();
-assert new File( basedir, 'target/docbkx/pdf/OpenICF-Docbook-Connector-1.4.0.0-Snapshot.pdf' ).exists();
-content = new File( basedir, 'target/docbkx/html/docbook-connector-1.4.0.0-SNAPSHOT/index.html' ).text;
+assert new File( basedir, 'target/docbkx/pdf/OpenICF-Docbook-Connector-1.4.0.0.pdf' ).exists();
+content = new File( basedir, 'target/docbkx/html/docbook-connector-1.4.0.0/index.html' ).text;
 
 assert content.contains( '"connectorName" : "org.forgerock.openicf.connectors.docbook.DocBookConnector"' );
 
