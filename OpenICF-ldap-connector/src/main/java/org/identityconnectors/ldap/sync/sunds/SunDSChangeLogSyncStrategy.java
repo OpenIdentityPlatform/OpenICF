1c608f0e864f620ba71df80c441eacb4f6e88c28
Gael Allioux <gael.allioux@forgerock.com>
2015-12-16 19:32:11 +0100

    OPENICF-402 - change number issue

--- OpenICF-ldap-connector/src/main/java/org/identityconnectors/ldap/sync/sunds/SunDSChangeLogSyncStrategy.java
+++ OpenICF-ldap-connector/src/main/java/org/identityconnectors/ldap/sync/sunds/SunDSChangeLogSyncStrategy.java
@@ -134,7 +134,7 @@ public class SunDSChangeLogSyncStrategy implements LdapSyncStrategy {
         final int[] processedChangeNumber = { -1 };
         
         if (token != null && log.isWarning()) {
-            if (currentChangeNumber[0] > getChangeLogAttributes().getLastChangeNumber()) {
+            if ((Integer) token.getValue() > getChangeLogAttributes().getLastChangeNumber()) {
                 //[OPENICF-402] The current SyncToken should never be greater than the lastChangeNumber in the changelog
                 // We log the issue and let the process go
                 log.warn("The current SyncToken value ({0}) is greater than the lastChangeNumber value ({1})", currentChangeNumber[0], getChangeLogAttributes().getLastChangeNumber());
